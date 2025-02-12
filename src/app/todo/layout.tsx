1fd7f4edb0736b27e0b033309d3c9a3c67889286
JuanLaspiur <laspiurperezjuan@gmail.com>
2025-02-12 18:53:47 -0300

    Header

--- src/app/todo/layout.tsx
+++ src/app/todo/layout.tsx
@@ -1,24 +1,13 @@
 'use client';
 
-import { useAuth } from "@/contexts/authcontext";
-import { FiLogOut } from "react-icons/fi"; 
-import { useRouter } from "next/navigation";
 import Calendar from "@/components/Calendar";
-import DarkModeToggle from "@/components/DarkModeToggle";
-import LanguageToggle from "@/components/LanguageToggle"; ""
-import { useLanguage } from "@/contexts/languageContext";
+import DarkModeToggle from "@/components/buttons/header/DarkModeToggle";
+import LanguageToggle from "@/components/buttons/header/LanguageToggle"; 
 import { useTheme } from "@/contexts/themeContext";
+import LogoutToggle from "@/components/buttons/header/LogoutToggle";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  const { logout } = useAuth();
-  const { language } = useLanguage();
   const { isDarkMode } = useTheme();
-  const router = useRouter();
-
-  const handleLogout = () => {
-    logout();
-    router.push("/login"); 
-  };
 
 
   return (
@@ -28,13 +17,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
           <LanguageToggle/>
           <DarkModeToggle/>
         </div>
-        <button
-          onClick={handleLogout}
-          className="flex items-center text-red-500 hover:text-red-700"
-        >
-          <FiLogOut className="mr-2" />
-          {language === "es" ? "Cerrar sesión" : "Logout"}
-        </button>
+       <LogoutToggle/>
       </header>
       <div className="flex flex-1">
         <aside className="w-1/4 p-4">
---
+++ src/components/buttons/header/LogoutToggle.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import { FiLogOut } from "react-icons/fi"; 
+import { motion } from 'framer-motion';
+import { useLanguage } from "@/contexts/languageContext";
+import { useRouter } from "next/navigation";
+import { useAuth } from "@/contexts/authcontext";
+
+const LogoutToggle: React.FC = () => {
+  const { logout } = useAuth();
+  const { language } = useLanguage();
+  const router = useRouter();
+
+  const handleLogout = () => {
+    logout();
+    router.push("/login"); 
+  };
+
+  return (
+    <motion.button
+      onClick={handleLogout}
+      className="flex items-center text-red-500 hover:text-red-700"
+      whileHover={{ scale: 1.1 }}
+      whileTap={{ scale: 0.9 }}
+      transition={{ type: "spring", stiffness: 300 }}
+    >
+      <FiLogOut className="mr-2" />
+      {language === "es" ? "Cerrar sesión" : "Logout"}
+    </motion.button>
+  );
+};
+
+export default LogoutToggle;
