42d8d6cca99e0f6f5c14fe138998b10558604b84
JuanLaspiur <laspiurperezjuan@gmail.com>
2025-02-13 15:22:09 -0300

    Guardar

---
+++ src/components/buttons/tasklist/AnimatedButton.tsx
@@ -0,0 +1,23 @@
+import { motion } from 'framer-motion';
+
+interface AnimatedButtonProps {
+  onClick: () => void;
+  buttonText: string;
+  isDarkMode: boolean;
+}
+
+const AnimatedButton = ({ onClick, buttonText, isDarkMode }: AnimatedButtonProps) => {
+  return (
+    <motion.button
+      onClick={onClick}
+      className={`px-4 py-2 ${isDarkMode ? 'bg-blue-600 hover:bg-blue-500' : 'bg-blue-500 hover:bg-blue-600'} text-white rounded-r`}
+      whileHover={{ scale: 1.1 }}
+      whileTap={{ scale: 0.9 }}
+      transition={{ type: "spring", stiffness: 300 }}
+    >
+      {buttonText}
+    </motion.button>
+  );
+};
+
+export default AnimatedButton;
