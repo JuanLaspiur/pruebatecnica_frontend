015138b8f0ea68382e76dd472ceb7d2ab15f05c8
JuanLaspiur <laspiurperezjuan@gmail.com>
2025-02-19 10:46:07 -0300

    save

--- src/components/subcomponents/registermodal/RegisterModalLayout.tsx
+++ src/components/subcomponents/registermodal/RegisterModalLayout.tsx
@@ -6,7 +6,7 @@ interface RegisterModalLayoutProps {
   const RegisterModalLayout = ({ children, onClose }: RegisterModalLayoutProps) => {
     return (
       <div className="w-full h-full absolute z-1 flex items-center justify-center bg-black bg-opacity-50">
-        <div className="bg-white p-6 rounded-lg shadow-lg w-full max-w-sm">
+        <div className="bg-white p-6 rounded-lg shadow-lg w-full max-w-sm mt-[-20px]">
           <h2 className="text-2xl font-semibold text-center text-blue-600 mb-4">Registro</h2>
           {children}
           <button onClick={onClose} className="mt-4 text-center text-sm text-gray-600 hover:underline w-full">
